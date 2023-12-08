896530fc601342bc2f3c63068547baec4070da88
kw <115936263+k-wlosek@users.noreply.github.com>
2023-12-08 13:07:16 +0100

    Day 8: Part 2

--- day8/solve.py
+++ day8/solve.py
@@ -1,4 +1,5 @@
 import re
+import math
 
 
 def parse_destinations(lines):
@@ -23,9 +24,29 @@ def solve_part1(destinations, instructions):
     return steps
 
 
+def solve_part2(destinations, instructions):
+    starting_positions = [x for x in destinations.keys() if x.endswith("A")]
+    ending_positions = [x for x in destinations.keys() if x.endswith("Z")]
+
+    # 1. Go through each starting position (until we hit an ending position) and find the cycle length
+    cycle_lengths = []
+    for start_pos in starting_positions:
+        current_pos = start_pos
+        cycle_length = 0
+        while current_pos not in ending_positions:
+            current_pos = destinations[current_pos][instructions[cycle_length % len(instructions)]]
+            cycle_length += 1
+        cycle_lengths.append(cycle_length)
+
+    # 2. Take the LCM of the cycle lengths
+    # This is the number of steps it would have taken, had we gone through all the cycles at the same time
+    return math.lcm(*cycle_lengths)
+
+
 if __name__ == "__main__":
-    with open("input") as f:
+    with open("testing") as f:
         lines = [line.strip() for line in f.readlines() if line.strip() != ""]
         instructions = list(lines.pop(0))
         destinations = parse_destinations(lines)
         print(solve_part1(destinations, instructions))
+        print(solve_part2(destinations, instructions))
